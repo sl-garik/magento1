bab8dbf82116a721b26bdfe647e033a402ed23d9
IgorS <sl-garik@rambler.ru>
2014-06-06 16:32:11 +0300

    update module "CustTotal"

--- www/app/code/local/IS/CustTotal/Model/Observer.php
+++ www/app/code/local/IS/CustTotal/Model/Observer.php
@@ -11,7 +11,7 @@ class IS_CustTotal_Model_Observer
             $data = $csv->getData($dir . DS . $file);
             for ($i = 1; $i < count($data); $i++) {
                 if (!empty($data[$i][0]) || !empty($data[$i][1])) {
-                    if ($i > 0){
+                    if ($i > 0) {
                         $bd_table = Mage::getModel('custtotal/custtotal');
                         $bd_table->setData('ZipCode', $data[$i][0]);
                         $bd_table->setData('NewPrice', $data[$i][1]);
